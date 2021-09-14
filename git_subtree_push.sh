eeba805aacf8938ad676110176d031f9bf878067
Cullen Lee <a410615903@gmail.com>
2021-09-13 18:32:36 -0700

    Updated git subtree process

--- git_subtree_push.sh
+++ git_subtree_push.sh
@@ -1,1 +1,1 @@
-git subtree push --prefix packages/universal_router https://github.com/keyskull/universal_router.git main
+git subtree push --prefix packages/universal_router https://github.com/keyskull/universal_router.git main --squash
