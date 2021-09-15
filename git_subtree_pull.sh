89b2cca677e2edecc4da4e63d68a41edf5f461e4
Cullen Lee <a410615903@gmail.com>
2021-09-14 22:32:19 -0700

    Updated workflows

--- git_subtree_pull.sh
+++ git_subtree_pull.sh
@@ -1,1 +1,1 @@
-git subtree pull --prefix packages/universal_router https://github.com/keyskull/universal_router.git main
+git subtree pull --prefix packages/universal_router https://github.com/keyskull/universal_router.git main --squash
